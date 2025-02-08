bd69c45ca6e1ca7fd436e947242fa5cada760576
warunsinx <warunsinx@gmail.com>
2025-02-08 19:44:34 +0700

    feat: setup pipe validation & enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from './config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(config.port);
   console.log(`Server is running on port ${config.port}`);
 }
