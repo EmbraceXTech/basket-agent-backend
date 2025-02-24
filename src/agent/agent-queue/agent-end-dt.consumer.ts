a72d1445dcb12dc9dbfeee665cd32a83dbe428f9
warunsinx <warunsinx@gmail.com>
2025-02-25 03:06:47 +0700

    feat: integrate agent end dt cron scheduler with agent stop func

--- src/agent/agent-queue/agent-end-dt.consumer.ts
+++ src/agent/agent-queue/agent-end-dt.consumer.ts
@@ -1,11 +1,22 @@
 import { Processor, WorkerHost } from '@nestjs/bullmq';
 import { Job } from 'bullmq';
 import { AGENT_END_DT_QUEUE } from 'src/constant/queue.constant';
+import { AgentService } from '../agent.service';
 
 @Processor(AGENT_END_DT_QUEUE)
 export class AgentEndDtConsumer extends WorkerHost {
+  constructor(private readonly agentService: AgentService) {
+    super();
+  }
+
   async process(job: Job<{ agentId: string }>) {
     const { agentId } = job.data;
-    console.log('agentId', agentId, 'AGENT_END_DT_QUEUE triggered !');
+    try {
+      await this.agentService.stop(agentId);
+      console.log('agentId', agentId, 'AGENT_END_DT_QUEUE triggered !');
+    } catch (error) {
+      console.log('agentId', agentId, 'AGENT_END_DT_QUEUE failed !');
+      console.error(error);
+    }
   }
 }
